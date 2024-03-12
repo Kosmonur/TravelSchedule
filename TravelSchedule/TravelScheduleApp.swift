06a24ba766dc9d63861181fd084a3eabe03f21c0
Aleksandr <125129763+Kosmonur@users.noreply.github.com>
2024-03-12 18:48:32 +0300

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Александр Пичугин on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
