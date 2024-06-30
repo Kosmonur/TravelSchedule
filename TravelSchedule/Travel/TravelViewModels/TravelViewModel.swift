14e90ef02634ceec8e0cf298732dc8ce6d4bb08a
Aleksandr <125129763+Kosmonur@users.noreply.github.com>
2024-07-01 01:54:39 +0300

    подключил сеть

--- TravelSchedule/Travel/TravelViewModels/TravelViewModel.swift
+++ TravelSchedule/Travel/TravelViewModels/TravelViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import OpenAPIURLSession
 
 final class TravelViewModel: ObservableObject {
     
@@ -31,6 +32,7 @@ final class TravelViewModel: ObservableObject {
         self.citiesViewModel = CitiesViewModel()
         self.routesViewModel = RoutesViewModel()
         self.storiesViewModel = StoriesViewModel(models: Constant.storiesData)
+        getStationsList()
     }
     
     func swapFromTo () {
@@ -39,5 +41,43 @@ final class TravelViewModel: ObservableObject {
     
     private func checkFindButtonVisibility() {
         findButtonIsHidden = from == Constant.from || to == Constant.to || from == Constant.to || to == Constant.from
+    }
+    
+    // Полный список станций
+    func getStationsList() {
+        let client = Client(
+            serverURL: try! Servers.server1(),
+            transport: URLSessionTransport()
+        )
+        
+        let service = StationsListService(
+            client: client,
+            apikey: apiKey
+        )
+        
+        Task {
+            do {
+                let stationsList = try await service.getStationsList()
+                let decodeList = try await Data(collecting: stationsList, upTo: 50*1024*1024)
+                let allStations = try JSONDecoder().decode(StationsList.self, from: decodeList)
+                print ("\nПолный список станций\n")
+                
+ //               let allCities = allStations.countries?.filter{$0.title == "Россия"}.map{$0.regions}.map{$0.map{$0.map{$0.settlements}}}.map{$0.map{$0.map{$0.map{$0.map{$0.title}}}}}
+                
+                let allRussia = allStations.countries?.filter{$0.title == "Россия"}
+                
+                let allRegions = allRussia.map{$0.map{$0.regions}}
+                
+                let allSettlements = allRegions.map{$0.map{$0.map{$0.map{$0.settlements}}}}
+                
+                let allCities = allSettlements.map{$0.compactMap{$0.map{$0.compactMap{$0.map{$0.compactMap{$0.title}}}}}}
+                
+                print (allCities)
+                
+                
+            } catch {
+                print("Error: \(error)")
+            }
+        }
     }
 }
